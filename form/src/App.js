c59ff3a3513afce188418c76933a5f4ee467d231
joel barrera <87788313+Windless95@users.noreply.github.com>
2022-09-07 13:22:43 -0400

    modal done

--- form/src/App.js
+++ form/src/App.js
@@ -1,4 +1,8 @@
-import "./App.css";
+import { React, Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { CheckIcon } from '@heroicons/react/24/outline'
+import Popup from './popup';
+
 
 function App() {
   return (
@@ -9,10 +13,10 @@ function App() {
             <div className="space-y-8 divide-y divide-gray-200">
               <div className="pt-8">
                 <div>
-                  <h3 className="text-lg font-medium leading-6 text-gray-900">
-                    Generic Text
+                  <h3 className="text-lg text-center font-medium leading-6 text-gray-900">
+                    Calculation
                   </h3>
-                  <p className="mt-1 text-sm text-gray-500">Generic Text</p>
+                  <p className="mt-1 text-sm text-gray-500 text-center">Demo</p>
                 </div>
                 <div className="mt-6 flex w-full gap-3">
                   <div className="flex-1">
@@ -20,7 +24,7 @@ function App() {
                       htmlFor="first-name"
                       className="block text-sm font-medium text-gray-700"
                     >
-                      First input
+                      Pre-suit settlement demand
                     </label>
                     <div className="mt-1">
                       <input
@@ -32,33 +36,13 @@ function App() {
                       />
                     </div>
                   </div>
-                  <div className>
-                    <label
-                      htmlFor="operation"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Operator
-                    </label>
-                    <div className="mt-1">
-                      <select
-                        id="operator"
-                        name="operator"
-                        autocomplete="operator"
-                        className="w-full rounded-md border-2 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                      >
-                        <option>X</option>
-                        <option>+</option>
-                        <option>-</option>
-                        <option>÷</option>
-                      </select>
-                    </div>
-                  </div>
+                  
                   <div className="flex-1">
                     <label
                       htmlFor="second"
-                      className="block text-sm font-medium text-gray-700"
+                      className="block text-sm px-2 font-medium text-gray-700"
                     >
-                      Second Input
+                      Pre-suit settlement offer
                     </label>
                     <div className="mt-1">
                       <input
@@ -70,34 +54,18 @@ function App() {
                       />
                     </div>
                   </div>
-                  <div className="flex-1">
-                    <label
-                      htmlFor="second"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Result
-                    </label>
-                    <div className="mt-1">
-                    <input
-                        type="text"
-                        name="second"
-                        id="second"
-                        autocomplete="second"
-                        className="block w-full rounded-md border-2 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                      />
-                    </div>
-                  </div>
+                  
                 </div>
               </div>
             </div>
-
             <div className="pt-5">
               <div className="flex justify-center">
                 <button
+                onclick={()=>Popup}
                   type="submit"
                   className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
-                  Get Results
+                  Calculate Percentage
                 </button>
               </div>
             </div>
@@ -108,4 +76,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
