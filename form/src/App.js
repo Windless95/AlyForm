aac7360071ea4eac16e7b2d42e504c9d9fb55880
joel barrera <87788313+Windless95@users.noreply.github.com>
2022-09-07 14:53:42 -0400

    zzz

--- form/src/App.js
+++ form/src/App.js
@@ -1,10 +1,29 @@
-import { React, Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import { CheckIcon } from '@heroicons/react/24/outline'
-import Popup from './popup';
-
+import { React, Fragment, useState, useEffect } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/24/outline";
 
 function App() {
+  const [open, setOpen] = useState(true);
+  const [input1, setInput1] = useState(" ")
+  const [input2, setInput2] = useState(" ")
+
+  const PopUp = ({ input1 = 0, input2 = 0 }) => {
+    const [difference, setDifference] = useState(0);
+
+    useEffect(() => {
+        setDifference(input2 - input1);
+    }, [input1, input2]);
+
+    return (
+        <>
+            <p className="text-sm text-gray-500">Difference: {difference}</p>
+            <p className="text-sm text-gray-500">Half of Difference: {difference * 0.5}</p>
+            <p className="text-sm text-gray-500">Quarter of Difference: {difference * 0.25}</p>
+        </>
+    )
+}
+
+
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <div className="border-solid border-10 m-8 flex h-3/4 w-3/4 items-center bg-gradient-to-bl from-cyan-400 via-white to-black">
@@ -24,10 +43,12 @@ function App() {
                       htmlFor="first-name"
                       className="block text-sm font-medium text-gray-700"
                     >
-                      Pre-suit settlement demand
+                      1
                     </label>
                     <div className="mt-1">
                       <input
+                        value={input1}
+                        onInput={(e) => setInput1(e.target.value)}
                         type="text"
                         name="first"
                         id="first"
@@ -36,16 +57,18 @@ function App() {
                       />
                     </div>
                   </div>
-                  
+
                   <div className="flex-1">
                     <label
                       htmlFor="second"
                       className="block text-sm px-2 font-medium text-gray-700"
                     >
-                      Pre-suit settlement offer
+                      2
                     </label>
                     <div className="mt-1">
                       <input
+                        value={input2}
+                        onInput={(e) => setInput2(e.target.value)}
                         type="text"
                         name="second"
                         id="second"
@@ -54,19 +77,80 @@ function App() {
                       />
                     </div>
                   </div>
-                  
                 </div>
               </div>
             </div>
             <div className="pt-5">
               <div className="flex justify-center">
                 <button
-                onclick={()=>Popup}
+                  onClick={() => setOpen((open) => !open)}
                   type="submit"
                   className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                   Calculate Percentage
                 </button>
+                <Transition.Root show={open} as={Fragment}>
+                  <Dialog as="div" className="relative z-40" onClose={setOpen}>
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-out duration-300"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="ease-in duration-200"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 z-10 overflow-y-auto">
+                      <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                        <Transition.Child
+                          as={Fragment}
+                          enter="ease-out duration-300"
+                          enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                          enterTo="opacity-100 translate-y-0 sm:scale-100"
+                          leave="ease-in duration-200"
+                          leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                          leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                          <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                            <div>
+                              <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                                <CheckIcon
+                                  className="h-6 w-6 text-green-600"
+                                  aria-hidden="true"
+                                />
+                              </div>
+                              <div className="mt-3 text-center sm:mt-5">
+                                <Dialog.Title
+                                  as="h3"
+                                  className="text-lg font-medium leading-6 text-gray-900"
+                                >
+                                  RESULT
+                                </Dialog.Title>
+                                <div className="mt-2">
+                                  <p className="text-sm text-gray-500">
+                                    answer
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                            <div className="mt-5 sm:mt-6">
+                              <button
+                                type="button"
+                                className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+                                onClick={() => setOpen(false)}
+                              >
+                                Go back Home
+                              </button>
+                            </div>
+                          </Dialog.Panel>
+                        </Transition.Child>
+                      </div>
+                    </div>
+                  </Dialog>
+                </Transition.Root>
               </div>
             </div>
           </form>
@@ -76,4 +160,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
